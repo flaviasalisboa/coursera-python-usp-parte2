0765f5efb71628813d3da2cff5a77fb77cd25c1d
flaviasalisboa <flavia.lisboa@gmail.com>
2020-06-15 14:55:20 -0300

    inclusão da pasta semana-1

---
+++ Semana-1/sem1-funcao-dimensoes-matriz.py
@@ -0,0 +1,12 @@
+def dimensoes(matriz):
+	numRows = len(matriz)
+	numCols = len(matriz[0])
+	print(numRows, end='')
+	print('X', end='')
+	print(numCols)
+
+#num = dimensoes([[1, 2, 3],
+#				 [3, 4, 5],
+#				 [5, 6, 8],
+#				 [9, 9, 9]])
+#print(num)
---
+++ Semana-1/sem1-funcao-soma_matrizes.py
@@ -0,0 +1,25 @@
+def soma_matrizes(m1, m2):
+	matrizSoma = []
+	numRows = len(m1) 
+	numCols = len(m1[0])
+	numRows2 = len(m2)
+	numCols2 = len(m2[0])
+	if (numRows == numRows2) and (numCols == numCols2):
+		for i in range(numRows):
+			# Cria uma nova linha na matriz_soma
+			matrizSoma.append([])
+			for j in range(numCols):
+				# Soman os elementos do mesmo índice e gravam na matrizSoma
+				soma = m1[i][j] + m2[i][j]
+				matrizSoma[i].append(soma)
+		print("soma_matrizes(m1, m2) =>", end=' ')
+		return matrizSoma
+	else:
+		print("soma_matrizes(m1, m2) =>", end=' ')
+		return False
+
+
+#m1 = [[1, 2, 3], [4, 5, 6]]
+#m2 = [[2, 3, 4], [5, 6, 7]]
+#soma = soma_matrizes(m1, m2)
+#print(soma)
